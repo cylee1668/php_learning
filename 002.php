152b0f7fb7f315bcc6d3d74e1efd672007ec9a54
cylee1668 <cylee1668@users.noreply.github.com>
2015-09-17 09:47:10 +0800

    Create 002.php

---
+++ 002.php
@@ -0,0 +1,20 @@
+#In PHP, all keywords (e.g. if, else, while, echo, etc.), classes, functions, and user-defined functions are NOT case-sensitive.
+
+<!DOCTYPE html>
+
+<html>
+<body>
+
+<?php
+ECHO "Hello world!<br>";
+echo "hello world!<br>";
+EcHo "hello world!<br>";
+
+
+?>
+
+
+
+
+</body>
+</html>
