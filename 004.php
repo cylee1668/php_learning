77ebb84d16b1c99cd1f49728c6ac2ba0dc43d092
cylee1668 <cylee1668@gmail.com>
2015-09-18 10:23:37 +0800

    18sep2015 tutorial

---
+++ 004.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+$txt = "w3School.com";
+echo "I love $txt !";
+echo "<br/><br/>";
+echo "I love " . $txt . " ! ";
+
+echo "<br/>";
+
+$x = 5;
+$y = 4;
+echo $x + $y;
+
+?>
+
+
+</body>
+</html>
---
+++ 005.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body>
+<?php
+echo "<h2>PHP is Fun!</h2>";
+echo " Hello world!<br>";
+echo "I'm abourt to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+
+$txt1 = "Learn PHP";
+$txt2 = "W3Schools.com";
+$x = 5;
+$y = 4;
+
+echo "<h2>" . $txt1 . "</h2>";
+echo "Study PHP at " . $txt2 . "<br>";
+echo $x + $y;
+
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br/>";
+print "I am learn about PHP! ";
+
+
+print "<h2>" . $txt1 . "</h2>";
+print "Study PHP at " . $txt2 . "<br>";
+print $x + $y;
+?>
+
+</body>
+</html>
